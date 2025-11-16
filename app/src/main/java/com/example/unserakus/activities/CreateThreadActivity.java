f02fb0fde6d0812968d809653e5e40c096296e7f
C1guu <rifaldi61104@gmail.com>
2025-11-16 19:47:30 +0700

    done ga bang

--- app/src/main/java/com/example/unserakus/activities/CreateThreadActivity.java
+++ app/src/main/java/com/example/unserakus/activities/CreateThreadActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -54,7 +55,7 @@ public class CreateThreadActivity extends AppCompatActivity {
         btnPostThread = findViewById(R.id.btnPostThread);
 
         ivPreview = findViewById(R.id.ivPreview);
-        btnPickImage = findViewById(R.id.btnPickImage);
+        ImageButton btnPickImage = findViewById(R.id.btnPickImage);
 
         // Setup Launcher
         imagePickerLauncher = registerForActivityResult(
